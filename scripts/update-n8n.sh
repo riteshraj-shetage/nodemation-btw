41e69c6a3ed51ff2f232333f9b1ab40512cbbb43
Riteshraj Shetage <144100040+riteshraj-shetage@users.noreply.github.com>
2025-12-16 21:34:16 +0530

    Add update script for n8n Docker container

---
+++ scripts/update-n8n.sh
@@ -0,0 +1,11 @@
+# Navigate to the directory containing your docker compose file
+# cd </path/to/your/compose/file/directory>
+
+# Pull latest version
+docker compose pull
+
+# Stop and remove older version
+docker compose down
+
+# Start the container
+docker compose up -d
